3e5b893f5ccedc9a6d70c0aeb9f99edf04917e7b
Joy <joymccaffreychicago@gmail.com>
2017-02-08 15:16:21 -0600

    Add seed data.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Task.create!([{name: "LinkedIn Jobs", description: "Access LinkedIn and search for jobs."}])
