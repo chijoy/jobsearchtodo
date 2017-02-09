7eb64698f0a16ff5c11c275cc6a98d9239a8da59
Joy <joymccaffreychicago@gmail.com>
2017-02-09 13:55:15 -0600

    Add routes and controller.

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,41 @@
 class TasksController < ApplicationController
+  def index
+  @tasks = Task.all
+  end
+
+  def new
+  @task = Task.new
+  end
+
+  def create
+  @task = Task.create(
+  name: params[:name],
+  description: params[:description])
+
+      redirect_to '/'
+  end
+
+  def show
+  @task =Task.find_by(id: params[:id])
+  end
+
+  def edit
+  @task =Task.find_by(id: params[:id])
+  end
+
+  def update
+  @task = Task.find_by(id: params[:id])
+  @task.update(
+  name: params[:name],
+  description: params[:description])
+
+    redirect_to "/tasks/#{@task.id}"
+  end
+
+  def destroy
+  @task = Task.find_by(id: params[:id])
+  @task.destroy
+
+    redirect_to '/'
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
-Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+Rails.application.routes.draw do 
+  get '/' => 'tasks#index'
+  get '/tasks' => 'tasks#index'
+  get '/tasks/new' => 'tasks#new'
+  post '/tasks' => 'tasks#create'
+
+  get '/tasks/:id' => 'tasks#show'
+
+  get '/tasks/:id/edit' => 'tasks#edit'
+  patch '/tasks/:id' => 'tasks#update'
+
+  delete '/tasks/:id' => 'tasks#destroy'
 end
